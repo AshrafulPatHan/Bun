a7eccd2067487493fd5fa6fc4c0db4c08a65bf51
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-04-25 13:32:38 +0600

    hero shared layout is complite

--- bun_officeal_website_clone/frontend/src/components/home/hero.tsx
+++ bun_officeal_website_clone/frontend/src/components/home/hero.tsx
@@ -5,14 +5,14 @@ import Hero_Install_bun from "@/ui/Hero_install_bun";
 
 export default function Hero_() {
     return (
-        <div className="flex-center mx-auto hero-background  ">
-            <div className="flex-row-center justify-center mx-auto max-w-7xl gap-20 mt-24 mb-32   ">
+        <div className="flex-center mx-auto hero-background pt-24 pb-32 ">
+            <div className="flex flex-row items-start justify-center mx-auto max-w-7xl gap-20 ">
                 <div className="w-[700px] ">
                     <button>Bun v1.3.12 is here! →</button>
                     <h1 className="text-5xl font-bold mt-4 mb-8">Bun is a fast JavaScript package manager</h1>
                     <p className="text-xl text-[#D1D5DB] mb-8 ">Bun is a fast, incrementally adoptable all-in-one JavaScript, TypeScript & JSX toolkit. Use individual tools like bun test or bun install in Node.js projects, or adopt the complete stack with a fast JavaScript runtime, bundler, test runner, and package manager built in. Bun aims for 100% Node.js compatibility.</p>
                     <p className="text-xl font-bold">Install Bun v1.3.12</p>
-                    <Hero_Install_bun/>
+                    <Hero_Install_bun />
                     <div>
                         <p> USED BY</p>
                         <img src={logos_} alt="company" />
@@ -21,10 +21,11 @@ export default function Hero_() {
                 <div className="w-[500px] flex-center gap-4">
                     <div>
                         {/* thear are a window */}
-                        <SharedLayoutAnimation/>
+                        <SharedLayoutAnimation />
                     </div>
                     <a href="https://github.com/rolldown/benchmarks/blob/134f936777397f5d1246b5ef8974f01e2f799690/apps/10000/package.json" target="_blank" rel="noopener noreferrer">View benchmark →</a>
                 </div>
-            </div></div>
+            </div>
+        </div>
     )
 }
--- bun_officeal_website_clone/frontend/src/ui/Shared_layout.tsx
+++ bun_officeal_website_clone/frontend/src/ui/Shared_layout.tsx
@@ -1,8 +1,117 @@
+import chart1 from "@/assets/image/54785.png"
+import chart2 from "@/assets/image/516165.png"
+import chart3 from "@/assets/image/1645665.png"
+import chart4 from "@/assets/image/45987.png"
+import { useState } from "react";
 
 export default function SharedLayoutAnimation() {
-    return(
-        <div>
-            
+    // css style
+    let onButton = "bg-[#282A36] py-4 px-6 text-xl rounded-t-md border-b-2 border-b-pink-400 ";
+    let offButton = " py-4 px-6 hover:bg-[#282A36] text-xl rounded-t-md border-b-2 border-b-mauve-700 hover:border-b-blue-500 transition-colors duration-300 ease-in-out ";
+    
+    // chart 1
+    function Chart1() {
+        return (
+            <div className="flex flex-col items-center">
+                <h3 className="text-2xl">Bundling 10,000 React components</h3>
+                <p className="mb-4">Build time in milliseconds (Linux x64, Hetzner)</p>
+                <div className="bg-[#282A36] p-1 rounded-lg ">
+                    <img src={chart1} alt="chart" />
+                </div>
+            </div>
+        )
+    }
+    
+    // chart 2
+    function Chart2() {
+        return (
+            <div className="flex flex-col items-center">
+                <h3 className="text-2xl">Express.js 'hello world'</h3>
+                <p className="mb-4">HTTP requests per second (Linux x64)</p>
+                <div className="bg-[#282A36] p-1 rounded-lg ">
+                    <img src={chart2} alt="chart" />
+                </div>
+            </div>
+        )
+    }
+    
+    // chart 3
+    function Chart3() {
+        return (
+            <div className="flex flex-col items-center">
+                <h3 className="text-2xl">Load a huge table</h3>
+                <p className="mb-4">Queries per second. 100 rows x 100 parallel queries</p>
+                <div className="bg-[#282A36] p-1 rounded-lg ">
+                    <img src={chart3} alt="chart" />
+                </div>
+            </div>
+        )
+    }
+    
+    // chart 4
+    function Chart4() {
+        return (
+            <div className="flex flex-col items-center">
+                <h3 className="text-2xl">WebSocket chat server</h3>
+                <p className="mb-4">Messages sent per second (Linux x64, 32 clients)</p>
+                <div className="bg-[#282A36] p-1 rounded-lg ">
+                    <img src={chart4} alt="chart" />
+                </div>
+            </div>
+        )
+    }
+    
+    // state of button style
+    const [button1, setButton1] = useState(onButton);// button 1 style
+    const [button2, setButton2] = useState(offButton);// button 2 style
+    const [button3, setButton3] = useState(offButton);// button 3 style
+    const [button4, setButton4] = useState(offButton);// button 4 style
+
+    // state of element
+    const [chart, setChart] = useState(Chart1()) // main chart
+
+    // handel button clike
+    const handelShared = (id:number)=> {
+        if (id === 1) {
+            setChart(Chart1);
+            setButton1(onButton);
+            setButton2(offButton);
+            setButton3(offButton);
+            setButton4(offButton);
+        }else if (id === 2) {
+            setChart(Chart2);
+            setButton1(offButton);
+            setButton2(onButton);
+            setButton3(offButton);
+            setButton4(offButton);
+        }else if (id === 3) {
+            setChart(Chart3);
+            setButton1(offButton);
+            setButton2(offButton);
+            setButton3(onButton);
+            setButton4(offButton);
+        }else if (id === 4) {
+            setChart(Chart4);
+            setButton1(offButton);
+            setButton2(offButton);
+            setButton3(offButton);
+            setButton4(onButton);
+        }else{
+            alert("error is comming")
+        }
+    }
+
+    return (
+        <div className="flex flex-col gap-4">
+            <div className="flex flex-row items-center justify-between">
+                <button onClick={()=>handelShared(1)} className={button1}>Bundler</button>
+                <button onClick={()=>handelShared(2)} className={button2}>Express</button>
+                <button onClick={()=>handelShared(3)} className={button3}>Postgres</button>
+                <button onClick={()=>handelShared(4)} className={button4}>WebSockets</button>
+            </div>
+            <div>
+                {chart}
+            </div>
         </div>
     )
 }
