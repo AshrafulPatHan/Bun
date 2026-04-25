95c25cca02d486c5860e6198f18c843ba595252d
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-04-25 10:46:45 +0600

    add togol install bun

--- bun_officeal_website_clone/frontend/src/components/home/hero.tsx
+++ bun_officeal_website_clone/frontend/src/components/home/hero.tsx
@@ -10,7 +10,7 @@ export default function Hero_() {
                 <div className="w-[700px] ">
                     <button>Bun v1.3.12 is here! →</button>
                     <h1 className="text-5xl font-bold mt-4 mb-8">Bun is a fast JavaScript package manager</h1>
-                    <p className="text-xl text-[#D1D5DB] ">Bun is a fast, incrementally adoptable all-in-one JavaScript, TypeScript & JSX toolkit. Use individual tools like bun test or bun install in Node.js projects, or adopt the complete stack with a fast JavaScript runtime, bundler, test runner, and package manager built in. Bun aims for 100% Node.js compatibility.</p>
+                    <p className="text-xl text-[#D1D5DB] mb-8 ">Bun is a fast, incrementally adoptable all-in-one JavaScript, TypeScript & JSX toolkit. Use individual tools like bun test or bun install in Node.js projects, or adopt the complete stack with a fast JavaScript runtime, bundler, test runner, and package manager built in. Bun aims for 100% Node.js compatibility.</p>
                     <p className="text-xl font-bold">Install Bun v1.3.12</p>
                     <Hero_Install_bun/>
                     <div>
--- bun_officeal_website_clone/frontend/src/ui/Hero_install_bun.tsx
+++ bun_officeal_website_clone/frontend/src/ui/Hero_install_bun.tsx
@@ -1,17 +1,33 @@
 import copyI from "@/assets/icon/copy.svg"
+import { useState } from "react"
 
 export default function Hero_Install_bun() {
+    const [scriptI,setScriptI] = useState("curl -fsSL https://bun.sh/install | bash") 
+
+    const handelInstallBun = (id:number) =>{
+        if (id === 1) {
+            setScriptI("curl -fsSL https://bun.sh/install | bash")
+        }else if (id === 2) {
+            setScriptI(`powershell -c "irm bun.sh/install.ps1 | iex"`)
+        }else{
+            alert("error is comming")
+        }
+    };
     return(
         <div className="flex flex-col items-start mt-3 mb-5">
             {/* button section */}
             <div className="flex flex-row items-center">
-                <button className="bg-[#F472B6] border-2 border-b-0 border-[#F472B6] text-base py-2 px-4 rounded-t-md ">Linux & macOS</button>
-                <button className="bg-[#3b3f4b] border-2 border-b-0 border-[#282A36] text-base py-2 px-4 rounded-t-md ">Windows</button>
-                <a className="ml-4 text-[#8a8e9c] " href="http://" target="_blank" rel="noopener noreferrer">View install script</a>
+                <button 
+                onClick={()=> handelInstallBun(1)}
+                className="bg-[#F472B6] border-2 border-b-0 border-[#F472B6] text-base py-2 px-4 rounded-t-md ">Linux & macOS</button>
+                <button 
+                onClick={()=> handelInstallBun(2)}
+                className="bg-[#3b3f4b] border-2 border-b-0 border-[#282A36] text-base py-2 px-4 rounded-t-md ">Windows</button>
+                <a className="ml-4 text-[#8a8e9c] " href="https://bun.sh/install" target="_blank" rel="noopener noreferrer">View install script</a>
             </div>
             {/* script section */}
             <div className="flex flex-row items-center justify-between w-full border-2 border-[#F472B6] bg-[#282A36] rounded-sm rounded-tl-none py-5 px-4 ">
-                <p>curl -fsSL https://bun.sh/install | bash</p>
+                <p>{scriptI}</p>
                 <button>
                     <img src={copyI} alt="copy icon" className="invert brightness-0" />
                 </button>
